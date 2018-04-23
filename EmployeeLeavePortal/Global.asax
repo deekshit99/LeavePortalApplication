8fa5650c55bad9a6e85a820513978c73a0edf72f
naga satyanand deekshit prakhya <prakhyadeekshit@gmail.com>
2018-04-23 11:45:59 -0400

    Add project files.

---
+++ EmployeeLeavePortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeLeavePortal.MvcApplication" Language="C#" %>
---
+++ EmployeeLeavePortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeLeavePortal.Startup))]
+namespace EmployeeLeavePortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
